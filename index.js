0012aa5e484faaf94511ffd26b4efa0d9fc9b80f
Justice <122542069+goob8079@users.noreply.github.com>
2025-12-29 14:17:51 +0900

    moved html files and update index.js

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-import axios from 'axios';
 import http from 'http';
+import fs from 'fs';
+
+const PORT  = 8080;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!',
-    }));
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './pages/';
+    switch(req.url) {
+        case '':
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/contact-me':
+            path += 'contact-me.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // not clarifying datatype makes data a Buffer (raw binary)
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end('Interal Server Error!');
+        } else {
+            // Node sends HTML directly to browser
+            res.end(data);
+        }
+    });
 });
 
-server.listen(8080);
+server.listen(PORT, () => {
+    console.log(`Server Port: ${PORT}`);
+});
